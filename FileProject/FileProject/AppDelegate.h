66635761626a5436d9c02b6e9f0e46997842d575
dxl <>
2019-07-26 22:44:25 +0900

    これは学習ファイルの操作に関するコードです。

---
+++ FileProject/FileProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FileProject
+//
+//  Created by dxl on 2019/7/26.
+//  Copyright © 2019年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FileProject/FileProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FileProject
+//
+//  Created by dxl on 2019/7/26.
+//  Copyright © 2019年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
