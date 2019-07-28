6edd8f30713426c545c8a98df65b841767aca56f
dxl <>
2019-07-28 23:52:12 +0900

    これはUIついての簡単なアプリです

---
+++ UI/UILabel/UILabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILabel
+//
+//  Created by dxl on 2019/7/28.
+//  Copyright © 2019年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI/UIWindow/UIWindow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIWindow
+//
+//  Created by dxl on 2019/7/28.
+//  Copyright © 2019年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
