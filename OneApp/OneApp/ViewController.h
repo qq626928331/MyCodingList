72f93f3d72c3fbe52b619aebbd2f7eea7943215d
dxl <>
2019-07-30 00:11:58 +0900

    ネットのアプリ練習のコードを真似します。

---
+++ OneApp/OneApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OneApp
+//
+//  Created by dxl on 2019/7/29.
+//  Copyright © 2019年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
